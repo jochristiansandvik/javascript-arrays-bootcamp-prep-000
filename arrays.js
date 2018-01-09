9227e6a57e2de5e4c71c6eccc6a43c930832576e
Jo Christian Sandvik <jochristian.sandvik@gmail.com>
2018-01-09 22:22:19 +0000

    automatically backed up by learn

---
+++ arrays.js
@@ -0,0 +1,24 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
+
+function addElementToBeginningOfArray(array, element) {
+  var NewArray = [element, ...array];
+  return NewArray;
+}  
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+}
+
+function addElementToEndOfArray(array, element) {
+  var NewArray = [...array, element];
+}  
+
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+}
+
+function accessElementInArray(array, index) {
+  return array[index];
+}
+
+function 
